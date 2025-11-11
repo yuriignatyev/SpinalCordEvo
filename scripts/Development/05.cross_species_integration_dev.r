136db1373476e6be7dc254d0059de49ae40c6641
Yuri Ignatyev <80233707+yuriignatyev@users.noreply.github.com>
2025-11-11 03:22:42 +0100

    Create 05.cross_species_integration_dev.r

---
+++ scripts/Development/05.cross_species_integration_dev.r
@@ -0,0 +1,43 @@
+#####!!!!!!!!!! INTEGRATING DEVELOPMENT DATA ACROSS FROG AND MOUSE
+
+library(Seurat)
+set.seed(54)
+
+
+
+## mark species
+mouse$species <- 'Mouse'
+frog_m$species <- 'Frog'     ## minimal add
+
+## leave only cardinal classes
+cardinal_classes <- c('dI1','dI2','dI3','dI4','dI5','dI6','V0','V1','V2a','V2b','V3','MNs')
+mouse  <- subset(mouse,  idents = cardinal_classes)
+frog_m <- subset(frog_m, idents = cardinal_classes)
+
+## integration
+sc.list <- list(frog_m, mouse)   ## NOTE: list() is safer than c() for Seurat objects
+
+sc.list <- lapply(sc.list, function(x) {
+  SCTransform(x, variable.features.n = 7000, verbose = FALSE)
+})
+
+features <- SelectIntegrationFeatures(object.list = sc.list, nfeatures = 7000)
+
+## remove Rgs/Rpl genes from features (cell-state genes)
+features <- setdiff(features, grep("^(Rgs|Rpl)", features, value = TRUE))  ## NOTE: same intent, safer
+
+sc.list <- PrepSCTIntegration(object.list = sc.list, anchor.features = features, verbose = FALSE)
+
+sc.anchors <- FindIntegrationAnchors(object.list = sc.list,
+                                     normalization.method = "SCT",
+                                     anchor.features = features,
+                                     verbose = FALSE)
+
+scint <- IntegrateData(anchorset = sc.anchors, normalization.method = "SCT", verbose = FALSE)
+scint <- RunPCA(scint, npcs = 50, verbose = FALSE)
+scint <- RunUMAP(scint, dims = 2:50, verbose = FALSE)
+
+saveRDS(scint, "Integration_DevFrogMouse_JustCardinalClassNeurons.rds")
+
+scint <- FindNeighbors(scint, dims = 2:50, verbose = FALSE)
+scint <- FindClusters(scint, resolution = 0.9, verbose = FALSE)
