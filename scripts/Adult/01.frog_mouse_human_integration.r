2059ac3fd147f79fd27da536e8f3b56b6a1ec78d
Yuri Ignatyev <80233707+yuriignatyev@users.noreply.github.com>
2025-12-01 16:16:37 +0100

    Update 01.frog_mouse_human_integration_cca.r

--- scripts/Adult/01.frog_mouse_human_integration.r
+++ scripts/Adult/01.frog_mouse_human_integration.r
@@ -1,60 +1,24 @@
-#!/usr/bin/env Rscript
+#Let's load the full list of neuronal objects arcross studies
+sc.list <- readRDS('/.../List_of_Individual_SpeciesStudy_Neuronal_Objects_FrogMouseHumanZebrafish_SCTrasformed_Ready_for_Integration.rds')
 
-# Load required libraries
-library(optparse)
-library(Seurat)
-library(glmGamPoi)
-
-# Define command line arguments
-option_list <- list(
-  make_option(c("--data_dir"), type="character", default=NULL, help="Path to the data directory", metavar="character")
-)
-
-# Parse command line arguments
-opt_parser <- OptionParser(option_list=option_list)
-opt <- parse_args(opt_parser)
-
-# Check if mandatory arguments are provided
-if (is.null(opt$data_dir)) {
-  stop("You must provide a data directory path with --data_dir")
-}
-
-# Set working directory
-setwd(opt$data_dir)
-
-
-
-frog <- readRDS('/nfs/scistore23/sweengrp/iignatev/Tetrapod_SC/Fig4/Adult_Frog_Dataset_Ready_For_Integration_Not_SCTransformed.rds')
-mouse <- readRDS('/nfs/scistore23/sweengrp/iignatev/Tetrapod_SC/Fig4/Adult_Mouse_KatheCourtine_Dataset_Ready_For_Integration_Not_SCTransformed.rds')
-ariel_neu <- readRDS('/nfs/scistore23/sweengrp/iignatev/Tetrapod_SC/Fig4/Adult_Mouse_RussLevine_Dataset_Ready_For_Integration_Not_SCTransformed.rds')
-yadav <- readRDS('/nfs/scistore23/sweengrp/iignatev/Tetrapod_SC/Fig4/Adult_Human_Dataset_Yadav_Ready_For_Integration_Not_SCTransformed.rds')
-gautier <- readRDS('/nfs/scistore23/sweengrp/iignatev/Tetrapod_SC/Fig4/Adult_Human_Dataset_Gautier_Ready_For_Integration_Not_SCTransformed.rds')
-
-ariel_neu$species <- 'Mouse'
-ariel_neu$coarse <- readRDS('/nfs/scistore23/sweengrp/iignatev/Tetrapod_SC/Fig4/mouse_russ_coarse_ids.rds')
-
-frog <- SCTransform(frog, vars.to.regress = c('nCount_RNA', 'nFeature_RNA'), variable.features.n = 10000,vst.flavor = "v2")
-mouse <- SCTransform(mouse, vars.to.regress =  c('nCount_RNA', 'nFeature_RNA', 'batch'), variable.features.n = 10000,vst.flavor = "v2")
-ariel_neu <- SCTransform(ariel_neu, vars.to.regress =  c('nCount_RNA', 'nFeature_RNA'), variable.features.n = 10000,vst.flavor = "v2")
-yadav <- SCTransform(yadav, vars.to.regress =  c('nCount_RNA', 'nFeature_RNA', 'sample'), variable.features.n = 10000,vst.flavor = "v2")
-gautier <- SCTransform(gautier, vars.to.regress =  c('nCount_RNA', 'nFeature_RNA'), variable.features.n = 10000,vst.flavor = "v2")
+frog <- sc.list[[1]]
+russ <- sc.list[[2]]
+kathe <- sc.list[[3]]
+gautier <- sc.list[[4]]
+yadav <- sc.list[[5]]
 
 
 frog.vf <- VariableFeatures(frog)
-mouse.vf <- VariableFeatures(mouse)
-ariel_neu.vf <- VariableFeatures(ariel_neu)
-yadav.vf <- VariableFeatures(yadav)
+russ.vf <- VariableFeatures(russ)
+kathe.vf <- VariableFeatures(kathe)
 gautier.vf <- VariableFeatures(gautier)
+yadav.vf <- VariableFeatures(yadav)
 
-int.features <- intersect(frog.vf, mouse.vf)
-int.features <- intersect(int.features, ariel_neu.vf)
-int.features <- intersect(int.features, yadav.vf)
-int.features <- intersect(int.features, gautier.vf)
+
+int.features <- Reduce(intersect, list(frog.vf, russ.vf, kathe.vf, gautier.vf, yadav.vf))
 
 # Combine data
-sc.list_1 <- c(frog, c(mouse, ariel_neu))
-sc.list_2 <- c(yadav, gautier)
-sc.list <- c(sc.list_1, sc.list_2)
+sc.list <- c(sc.list[[1]], sc.list[[2]], sc.list[[3]], sc.list[[4]], sc.list[[5]]) 
 
 # Prepare for integration
 sc.list.prep <- PrepSCTIntegration(object.list = sc.list, anchor.features = int.features, assay = 'SCT')
@@ -71,4 +35,4 @@ sc.integrated <- RunUMAP(sc.integrated, dims = 1:150)
 sc.integrated <- FindNeighbors(sc.integrated, dims = 1:150)
 
 # Clean up metadata and Save results
-saveRDS(sc.integrated, 'Neurons_Integrated_Across_FrogMouseHuman_CCA.rds')
+saveRDS(sc.integrated, '/.../Neurons_Integrated_Across_FrogMouseHuman_CCA.rds')
