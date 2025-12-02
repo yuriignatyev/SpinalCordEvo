36028eebf713ace0f121a0a05d3d71b45575aae3
Yuri Ignatyev <80233707+yuriignatyev@users.noreply.github.com>
2025-12-02 02:54:35 +0100

    Update and rename 01.frog_mouse_human_integration.r to 01.cross_species_integration_adult.r

--- scripts/Adult/01.cross_species_integration_adult.r
+++ scripts/Adult/01.cross_species_integration_adult.r
@@ -8,6 +8,8 @@ russ <- sc.list[[2]]
 kathe <- sc.list[[3]]
 gautier <- sc.list[[4]]
 yadav <- sc.list[[5]]
+## if you want to add zebrafish as well - see Fig4. L-N, it is stored in the list of object as [[6]]  
+# zebrafish <- sc.list[[6]]
 
 
 frog.vf <- VariableFeatures(frog)
@@ -15,12 +17,14 @@ russ.vf <- VariableFeatures(russ)
 kathe.vf <- VariableFeatures(kathe)
 gautier.vf <- VariableFeatures(gautier)
 yadav.vf <- VariableFeatures(yadav)
-
+# zebrafish.vf <- VariableFeatures(zebrafish)
 
 int.features <- Reduce(intersect, list(frog.vf, russ.vf, kathe.vf, gautier.vf, yadav.vf))
+# int.features <- Reduce(intersect, list(frog.vf, russ.vf, kathe.vf, gautier.vf, yadav.vf, zebrafish.vf))
 
 # Combine data
 sc.list <- c(sc.list[[1]], sc.list[[2]], sc.list[[3]], sc.list[[4]], sc.list[[5]]) 
+# sc.list <- c(sc.list[[1]], sc.list[[2]], sc.list[[3]], sc.list[[4]], sc.list[[5]], sc.list[[6]]) 
 
 # Prepare for integration
 sc.list.prep <- PrepSCTIntegration(object.list = sc.list, anchor.features = int.features, assay = 'SCT')
@@ -28,7 +32,7 @@ sc.list.prep <- PrepSCTIntegration(object.list = sc.list, anchor.features = int.
 # Find integration anchors, for integration via RPCA change reduction to reduction = 'rpca'
 sc.anchors <- FindIntegrationAnchors(object.list = sc.list.prep, normalization.method = "SCT", anchor.features = int.features, reduction = 'cca')
 
-# Integrate data, for integration via RPCA change dims and npcs to 50
+# Integrate data, for integration via RPCA, change dims and npcs to 50
 sc.integrated <- IntegrateData(anchorset = sc.anchors, normalization.method = "SCT",  dims=1:150)
 
 # Run PCA, UMAP, and find neighbors
